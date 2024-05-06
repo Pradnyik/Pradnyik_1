d6860bf1535d593b5bdd28af05e1059399713c4a
Pradnyik <142507602+Pradnyik@users.noreply.github.com>
2024-05-06 19:54:55 +0530

    Add files via upload

---
+++ pradnyik.py
@@ -0,0 +1,7 @@
+def sum_of_numbers(num1, num2):
+    return num1 + num2
+
+number1 = 10
+number2 = 20
+result = sum_of_numbers(number1, number2)
+print("The sum of", number1, "and", number2, "is:", result)
